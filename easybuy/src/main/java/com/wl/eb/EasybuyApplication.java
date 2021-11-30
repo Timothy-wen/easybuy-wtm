f11c634dc5d363bb1028f939f084628970c19a66
TimothyWen <2287141954@qq.com>
2021-11-30 21:03:31 +0800

    1130创建项目仓库

---
+++ easybuy/src/main/java/com/wl/eb/EasybuyApplication.java
@@ -0,0 +1,13 @@
+package com.wl.eb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybuyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasybuyApplication.class, args);
+    }
+
+}
