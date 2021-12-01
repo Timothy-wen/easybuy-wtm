2593565c0da0fa9161728360bbc72105b680b2e4
TimothyWen <2287141954@qq.com>
2021-12-01 22:30:36 +0800

    1130创建项目仓库

---
+++ easybuy/src/main/java/com/wl/eb/mapper/EbNewsMapper.java
@@ -0,0 +1,9 @@
+package com.wl.eb.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wl.eb.module.EbNews;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface EbNewsMapper extends BaseMapper<EbNews> {
+}
---
+++ easybuy/src/main/java/com/wl/eb/mapper/EbOrderMapper.java
@@ -0,0 +1,10 @@
+package com.wl.eb.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wl.eb.module.EbOrder;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface EbOrderMapper extends BaseMapper<EbOrder> {
+}
