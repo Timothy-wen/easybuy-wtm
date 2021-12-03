adcaa63eaf4b9fa2797dec79106edac318bd9300
TimothyWen <2287141954@qq.com>
2021-12-03 14:00:36 +0800

    图片替换、分页

--- easybuy/src/main/java/com/wl/eb/controller/IndexController.java
+++ easybuy/src/main/java/com/wl/eb/controller/IndexController.java
@@ -1,11 +1,13 @@
 package com.wl.eb.controller;
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wl.eb.service.IndexService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 import java.util.Map;
@@ -17,13 +19,15 @@ public class IndexController {
     private IndexService indexService;
 
     @GetMapping("/index")//决定网页地址第二级
-    public String index(Model model){
-        System.out.println("====IndexController方法index====");
+    public String index(Model model,
+                        @RequestParam(value = "page",defaultValue = "1") int page){
+        System.out.println("====IndexController方法index,page:"+page+"====");
         //查询首页需要的数据
-        Map<String,Object> result = indexService.index();
+        Map<String,Object> result = indexService.index(page);
         System.out.println("====result:==="+result);
         //数据装入model
-        model.addAttribute("result",result);
+        model.addAttribute("productPage", (Page)result.get("ebProductPage"));
+
         //返回view
         return "index";
     }
--- easybuy/src/main/java/com/wl/eb/service/IndexService.java
+++ easybuy/src/main/java/com/wl/eb/service/IndexService.java
@@ -13,11 +13,11 @@ public class IndexService {
     @Resource
     private EbProductMapper ebProductMapper;
 
-    public Map<String,Object> index(){
+    public Map<String,Object> index(int psize){
         System.out.println("====IndexService方法index====");
         Map<String,Object> result=new HashMap();
         //分页查询1，8条数据
-        Page page = new Page(1,8);
+        Page page = new Page(psize,8);
         Page ebProductPage = ebProductMapper.selectPage(page,null);
         //装入result返回Controller
         result.put("ebProductPage",ebProductPage);
